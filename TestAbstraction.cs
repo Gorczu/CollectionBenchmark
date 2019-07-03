b52556236961a9c08bd70ec9d88a31f2ada8df8f
kgorczow <krzysztof.gorczowski@capgemini.com>
2019-07-03 21:33:15 +0200

    small refactor + added some tests

--- TestAbstraction.cs
+++ TestAbstraction.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
-
+using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
-
+using System.Linq;
 using Collections.Benchmarks;
 
 namespace Collections_Benchmarks
@@ -18,10 +18,13 @@ namespace Collections_Benchmarks
         public int N;
         List<DataStruct> structList = new List<DataStruct> ();
         LinkedList<DataStruct> linkedList = new LinkedList<DataStruct> ();
-        HashSet<DataStruct> set = new HashSet<DataStruct> ();
+        HashSet<DataStruct> set = null;
         DataStruct[] array = null;
+
+        DataClass [] arrayObjects = null;
+
         ConcurrentQueue<DataStruct> concurrent = null;
-        private ImmutableList<DataStruct> structImmutableList;
+        ImmutableList<DataStruct> structImmutableList;
 
         [GlobalSetup]
         public void Setup ()
@@ -37,9 +40,50 @@ namespace Collections_Benchmarks
                         D = rn.Next ()
                 });
             }
+            
             this.array = structList.ToArray ();
+            this.set = new HashSet<DataStruct>(this.array);
             this.structImmutableList = ImmutableList.Create<DataStruct> (structList.ToArray ());
             this.concurrent = new ConcurrentQueue<DataStruct> (structList);
+            this.arrayObjects = structList.Select(t =>  new DataClass(){ A = t.A, B=t.B, C=t.A, D=t.D })
+                                          .ToArray();
+            
+        }
+
+        [Benchmark]
+        public int Array_Bench ()
+        {
+            return ArrayCalculation (structList);
+        }
+
+        [Benchmark]
+        public int Array_NoInlining_Bench ()
+        {
+            return ArrayNoInlineCalculation (arrayObjects);
+        }
+
+        [Benchmark]
+        public int OneLinq_Bench ()
+        {
+            return structList.Sum(t => Calculate(t));
+        }
+
+        [Benchmark]
+        public int List_Bench ()
+        {
+            return ListCalculation (structList);
+        }
+
+        [Benchmark]
+        public int HashSet_Bench ()
+        {
+            return HashSetCalculation (this.set);
+        }
+
+        [Benchmark]
+        public int ISet_Bench ()
+        {
+            return ISetCalculation (this.set);
         }
 
         [Benchmark]
@@ -61,18 +105,6 @@ namespace Collections_Benchmarks
         }
 
         [Benchmark]
-        public int List_Bench ()
-        {
-            return ListCalculation (structList);
-        }
-
-        [Benchmark]
-        public int Array_Bench ()
-        {
-            return ArrayCalculation (structList);
-        }
-
-        [Benchmark]
         public int ConcurrentQueue_Bench ()
         {
             return ConcurrentQueueCalculation (concurrent);
@@ -89,17 +121,37 @@ namespace Collections_Benchmarks
             int result = 0;
             foreach (var item in concurrent)
             {
-                result += item.A + item.B / item.C + item.D;
+                result += Calculate(item);
             }
             return result;
         }
 
+        private int HashSetCalculation(HashSet<DataStruct> set)
+        {
+            int result = 0;
+            foreach (var item in concurrent)
+            {
+                result += Calculate(item);
+            }
+            return result;
+        }
+
+        private int ISetCalculation(ISet<DataStruct> set)
+        {
+            int result = 0;
+            foreach (var item in concurrent)
+            {
+                result += Calculate(item);
+            }
+            return result;
+        }
+        
         private int ConcurrentQueueCalculation (ConcurrentQueue<DataStruct> queue)
         {
             int result = 0;
             foreach (var item in queue)
             {
-                result += item.A + item.B / item.C + item.D;
+                result += Calculate(item);
             }
             return result;
         }
@@ -110,50 +162,72 @@ namespace Collections_Benchmarks
             for (int idx = 0; idx < N; idx++)
             {
                 var item = array[idx];
-                result += item.A + item.B / item.C + item.D;
+                result += Calculate(item);
             }
             return result;
         }
 
-        int IReadOnlyListCalculation (IReadOnlyList<DataStruct> items)
+        private int IReadOnlyListCalculation (IReadOnlyList<DataStruct> items)
         {
             int result = 0;
             foreach (var item in items)
             {
-                result += item.A + item.B / item.C + item.D;
+                result += Calculate(item);
             }
             return result;
         }
 
-        int ImmutableListCalculation (ImmutableList<DataStruct> items)
+        private int ImmutableListCalculation (ImmutableList<DataStruct> items)
         {
             int result = 0;
             foreach (var item in items)
             {
-                result += item.A + item.B / item.C + item.D;
+                result += Calculate(item);
             }
             return result;
         }
 
-        int IImmutableListCalculation (IImmutableList<DataStruct> items)
+        private int IImmutableListCalculation (IImmutableList<DataStruct> items)
         {
             int result = 0;
             foreach (var item in items)
             {
-                result += item.A + item.B / item.C + item.D;
+                result += Calculate(item);
             }
             return result;
         }
 
-        int ListCalculation (List<DataStruct> items)
+        private int ListCalculation (List<DataStruct> items)
         {
             int result = 0;
             foreach (var item in items)
             {
-                result += item.A + item.B / item.C + item.D;
+                result += Calculate(item);
             }
             return result;
 
+        }
+
+        private int ArrayNoInlineCalculation(DataClass[] arrayObjects)
+        {
+            int result = 0;
+            for (int idx = 0; idx < N; idx++)
+            {
+                var item = arrayObjects[idx];
+                result += CalculateFromObj(item);
+            }
+            return result;
+        }
+
+        private int CalculateFromObj(DataClass data)
+        {
+           return (data.A + data.B) - (data.C + data.D);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int Calculate(DataStruct data)
+        {
+            return (data.A + data.B) - (data.C + data.D);
         }
     }
 }
